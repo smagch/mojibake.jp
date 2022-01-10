23d0198ec78d5eee6fca92250c10e38a4df663f0
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-10 15:12:39 +0900

    Add google tag manager settings

---
+++ additional.d.ts
@@ -0,0 +1,18 @@
+
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
+}
+
+interface Window {
+  dataLayer?: object[];
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GTM_ID: string;
+  }
+}
