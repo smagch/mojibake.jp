e177cc15ecf312937361123b2fe8a423b6912556
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-10 15:49:08 +0900

    Add sentry and svgr

--- next.config.js
+++ next.config.js
@@ -1,4 +1,52 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const moduleExports = {
+  webpack5: false,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      };
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+        "url-loader",
+      ],
+    });
+
+    return config;
+  },
+};
+
+if (process.env.CI) {
+  module.exports = moduleExports;
+} else {
+  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
 }
