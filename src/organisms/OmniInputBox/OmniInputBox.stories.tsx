2d008ec0c364fd13eacbd7c0a2d62a45d0e077ca
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-13 14:10:08 +0900

    Add omniinput box

---
+++ src/organisms/OmniInputBox/OmniInputBox.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import OmniInputBox from "./OmniInputBox";
+
+export const Demo = () => {
+  return <OmniInputBox />;
+};
+
+export default {
+  title: "organisms/OmniInputBox/OmniInputBox",
+  component: OmniInputBox,
+};
---
+++ src/organisms/OmniInputBox/OmniInputBox.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styles from "./OmniInputBox.module.scss";
+
+const OmniInputBox = () => {
+  return <div className={styles.container}>hoge</div>;
+};
+
+export default OmniInputBox;
