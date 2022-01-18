24902d1ed47ba548ffedff95d506ae28b6dd8374
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-18 14:44:27 +0900

    Add FAQ section

---
+++ src/organisms/FAQSection/FAQSection.stories.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import FAQSection from "./FAQSection";
+
+export const Demo = () => {
+  return (
+    <div style={{ width: "600px", margin: "24px auto" }}>
+      <FAQSection />
+    </div>
+  );
+};
+
+export default {
+  title: "organisms/FAQSection",
+  component: FAQSection,
+};
---
+++ src/organisms/FAQSection/FAQSection.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Details, Summary } from "atoms/Details";
+import styles from "./FAQSection.module.scss";
+
+const FAQSection = () => {
+  return (
+    <section className={styles.section}>
+      <header className={styles.header}>
+        <h3>FAQ</h3>
+        <p>よくあるご質問</p>
+      </header>
+      <div className={styles.list}>
+        <Details>
+          <Summary>mojibake.jp は安全ですか？</Summary>
+          <p>
+            Mojibake
+            では、ファイルをサーバーにアップロードすることなく、ウェブブラウザ内部の機能でテキストの解析・変換をしています。
+            そのため通信による情報漏えいのリスクを負うことなく、安心してご利用いただけます。
+          </p>
+        </Details>
+        <Details>
+          <Summary>正常に動作しません</Summary>
+          <p>
+            <a href="https://browsehappy.com/">最新のブラウザ</a>
+            をインストールされていることをご確認ください。
+          </p>
+          <p>
+            文字化けの変換に最新の Web
+            ブラウザの機能を使っているため、古いブラウザでは正しく動作しない可能性があります。
+            それでも解決しない場合は、こちらのフォームより不具合をご報告いただけますと幸いです。
+          </p>
+        </Details>
+        <Details>
+          <Summary>文字化けの解析の仕組みを教えてください</Summary>
+          [文字コード]が UTF-8 っぽかったら、Shift JIS に変換します。Shift JIS
+          っぽいファイルなら、UTF-8 に変換します。
+        </Details>
+        <Details>
+          <Summary>文字化けを解読できませんでした</Summary>
+          ご不便をおかけして申し訳ありません。 Mojibake
+          は常にサービスの質の向上に努めております。
+          差し支えなければ、[こちらのフォーム]より不具合をご報告いただけますと幸いです。
+        </Details>
+      </div>
+    </section>
+  );
+};
+
+export default FAQSection;
