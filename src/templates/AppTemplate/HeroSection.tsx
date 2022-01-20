530a058e32d3f569aca1de0a5d2b1078b6492657
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-20 10:26:07 +0900

    Add seo contents

--- src/templates/AppTemplate/HeroSection.tsx
+++ src/templates/AppTemplate/HeroSection.tsx
@@ -11,10 +11,12 @@ const HeroSection = () => {
       </div>
       <section className={styles.container}>
         <header>
-          <h1 className={styles.h1}>文字化けを解消しましょう</h1>
+          <h1 className={styles.h1}>
+            文字化けしたファイルを元通りにできます。
+          </h1>
           <p className={styles.description}>
             Mojibake
-            は、文字化けを解読する変換アプリです。文字化けの原因を自動的に特定し、元の日本語に復元します。
+            は、文字化けしたテキストファイルの修復ができる変換ツールです。文字化けの原因を自動的に特定し、正しい日本語に復元します。
           </p>
         </header>
       </section>
