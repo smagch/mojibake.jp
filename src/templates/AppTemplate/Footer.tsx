b1053e74b85f260cf09db2a1543022a2b0d37261
Tomoya Shimaguchi <smagch@gmail.com>
2022-01-22 00:20:51 +0900

    Fix style

--- src/templates/AppTemplate/Footer.tsx
+++ src/templates/AppTemplate/Footer.tsx
@@ -4,8 +4,12 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      © 2022 by Shimaguchi Tomoya — Follow me on twitter:
-      <a href="https://twitter.com/smagch">@smagch</a>
+      <div>
+        <span>© 2022 by Shimaguchi Tomoya</span> —{" "}
+        <span>
+          Follow me on twitter: <a href="https://twitter.com/smagch">@smagch</a>
+        </span>
+      </div>
     </footer>
   );
 };
